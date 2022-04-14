df851c2d77a0677b8e9759ab19c1d3d5ad804e63
sheraz khan <sheraz.0403889@talmeez.pk>
2022-04-15 00:33:38 +0500

    dice value change

--- src/component/Dice/index.js
+++ src/component/Dice/index.js
@@ -1,26 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import "../../App.css"
 import DiceFaces from '../DiceFaces/index';
 const Dice = () => {
   const [isRolling, setIsRolling] = useState(false)
   const [rollValue, setRollValue] = useState(1)
   const rollDice = () => {
-    setRollValue(null);
     setIsRolling(true);
+    const interval = setInterval(() => {
+      setRollValue(Math.floor(Math.random() * 6) + 1)
+    }, 500)
 
     setTimeout(() => {
-      const newValue = Math.floor(Math.random() * 6) + 1;
-      setRollValue(newValue)
+      clearInterval(interval)
       setIsRolling(false)
     }, 5000)
   }
-  useEffect(()=>{
-    if(isRolling){
-      setTimeout(() => {  
-          setRollValue(Math.floor(Math.random() * 6) + 1)
-      }, 150)
-    }
-  },[rollValue,isRolling])
   return (
     <div className="container mx-auto m-8   my-20px">
       <div className='mx-auto'>
@@ -29,10 +23,10 @@ const Dice = () => {
       <div className="ml-4">
         <div>
           <button className="text-center w-32 border btn btn-primary  my-5" disabled={isRolling} onClick={rollDice}>
-            {isRolling ?`isRolling`:`Throw dice`}
-            </button>
+            {isRolling ? `isRolling ...` : `Throw dice`}
+          </button>
         </div>
-        <p className="result">Current RollValue:<span className="span"> {isRolling?<></>: rollValue}</span></p>
+        <p className="result">Current RollValue:<span className="span"> {isRolling ? <></> : rollValue}</span></p>
         <div>
         </div>
       </div>
--- src/component/DiceFaces/index.js
+++ src/component/DiceFaces/index.js
@@ -3,12 +3,14 @@ import "../../App.css"
 import Dots from "./Dots";
 const DiceFaces = ({ rollValue, isRolling }) => {
     const current = array.find((item) => item.rollValue === rollValue);
-    return (
+    return ( 
+        <div className="card mx-auto " >
                 <table className="border border-black rounded-md p-48  mx-auto " >
                     <tbody >
                         <Dots data={current?.arrbits} />
                     </tbody>
                 </table>
+                </div>          
     )
 }
 
