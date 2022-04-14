634ccdd67af56c98d8f34d631a658073bb9c5c7c
sheraz khan <sheraz.0403889@talmeez.pk>
2022-04-14 23:06:50 +0500

    dice random show value done

--- src/component/Dice/index.js
+++ src/component/Dice/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "../../App.css"
 import DiceFaces from '../DiceFaces/index';
 const Dice = () => {
@@ -12,8 +12,15 @@ const Dice = () => {
       const newValue = Math.floor(Math.random() * 6) + 1;
       setRollValue(newValue)
       setIsRolling(false)
-    }, 2000)
+    }, 5000)
   }
+  useEffect(()=>{
+    if(isRolling){
+      setTimeout(() => {  
+          setRollValue(Math.floor(Math.random() * 6) + 1)
+      }, 150)
+    }
+  },[rollValue,isRolling])
   return (
     <div className="container mx-auto m-8   my-20px">
       <div className='mx-auto'>
@@ -23,7 +30,7 @@ const Dice = () => {
         <div>
           <button className="text-center w-32 border btn btn-primary  my-5" disabled={isRolling} onClick={rollDice}>Throw dice</button>
         </div>
-        <p className="result">Last throw:<span className="span"> {rollValue}</span></p>
+        <p className="result">Current RollValue:<span className="span"> {isRolling?<></>: rollValue}</span></p>
         <div>
         </div>
       </div>
--- src/component/DiceFaces/index.js
+++ src/component/DiceFaces/index.js
@@ -1,25 +1,14 @@
 import { array } from "../Dice_Sides_data";
-import { Preloader, Oval } from 'react-preloader-icon';
 import "../../App.css"
 import Dots from "./Dots";
 const DiceFaces = ({ rollValue, isRolling }) => {
     const current = array.find((item) => item.rollValue === rollValue);
     return (
-        <div className="card">
-            {isRolling ? <Preloader
-                use={Oval}
-                size={100}
-                strokeWidth={25}
-                strokeColor="#0066ff"
-                duration={2000}
-                className='spiner'
-            />:
                 <table className="border border-black rounded-md p-48  mx-auto " >
                     <tbody >
                         <Dots data={current?.arrbits} />
                     </tbody>
-                </table>}
-        </div>
+                </table>
     )
 }
 
